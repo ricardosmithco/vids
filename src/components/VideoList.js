c31b73fca96a4c398af6ffa01603397ff3dce9bb
redelization <ricardosmithco@gmail.com>
2019-10-14 15:12:36 -0500

    created VideoDetail component and addressed cannot find snippet of null error by using if statement to show loading..., while waiting for video to be returned from youtube api

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({videos, onVideoSelect}) => {
+const VideoList = ({ videos, onVideoSelect }) => {
     const renderedList = videos.map((video) =>{
         return <VideoItem vid={video} onVideoSelect={onVideoSelect}/>;
     });
